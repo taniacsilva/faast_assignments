63eee7b329d4ad22da2cddeaca3ad9cf526843c8
taniacsilva <taniacsilva@github.com>
2023-05-16 12:15:09 +0100

    fix typo in test_cleaning.py

--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -5,7 +5,7 @@ import pandas as pd
 from life_expectancy.cleaning import clean_data
 
 
-def test_cleaning(pt_life_expectancy_expected, eu_life_expectancy_raw_expected):
+def test_cleaning(pt_life_expectancy_expected, eu_life_expectancy_input_expected):
     """Compare the output of the "clean_data" function to the expected output
         Args:
             pt_life_expectancy_expected (Fixture): load the expected output of the cleaning script
@@ -14,7 +14,7 @@ def test_cleaning(pt_life_expectancy_expected, eu_life_expectancy_raw_expected):
     """
     # Call the clean_data function and get the result
     clean_data_output_actual = clean_data(
-        eu_life_expectancy_raw_expected,
+        eu_life_expectancy_input_expected,
         region = 'PT'
     ).reset_index(drop=True)
 
